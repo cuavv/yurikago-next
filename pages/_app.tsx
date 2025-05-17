fc9fa59ebeffa65245301a3c8727084b6a2e0753
krabbe16 <57534374+krabbe16@users.noreply.github.com>
2025-05-17 20:32:26 +0900

    Revert "Add: 記事内のURLをカードで表示する"
    This reverts commit 206f0f7222d3f53613210926f7e41ee877b78ced.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'github-markdown-css'
-import '~/styles/remark-link-card.css'
 import usePageview from '~/hooks/usePageview'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
